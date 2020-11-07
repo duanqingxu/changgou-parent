c66c5991138d5f6923c578e4ff3bee5f725284ce
duanqingxu <1300501693@qq.com>
2020-11-07 11:48:16 +0800

    第三天学习，spu+sku的上传，以及商品的上架和下架

--- changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -1,17 +1,30 @@
 package com.changgou;
 
+import entity.IdWorker;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-@MapperScan(basePackages = {"com.changgou.dao"})
+@MapperScan(basePackages = {"com.changgou.goods.dao"})
 public class GoodsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class,args);
+    }
+
+    /***
+     * IdWorker
+     * @return
+     */
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(0,0);
     }
 }
